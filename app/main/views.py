8c37e33f2346d0a435cf897affa0ff9dc4ff9e98
luna825 <luna825@qq.com>
2015-12-29 09:19:45 +0800

    首页文章分页功能

--- app/main/views.py
+++ app/main/views.py
@@ -1,19 +1,22 @@
 # -*- coding: UTF-8 -*-
-from flask import render_template
+from flask import render_template,request,current_app
 from . import main
 from ..models import Post
 
 @main.route('/')
 def index():
-	posts = Post.query.order_by(Post.timestamp.desc()).all()
-	return render_template('index.html',posts=posts,digest=True)
+    page = request.args.get('page',1,int)
+    pagination = Post.query.order_by(Post.timestamp.desc()).paginate(
+        page,per_page=current_app.config['FLASK_POSTS_PER_PAGE'],error_out=False)
+    posts = pagination.items
+    return render_template('index.html',posts=posts,digest=True,pagination=pagination)
 
 @main.route('/post/<int:id>')
 def post(id):
-	post = Post.query.get_or_404(id)
-	return render_template('post.html',posts=[post])
+    post = Post.query.get_or_404(id)
+    return render_template('post.html',posts=[post])
 
 @main.route('/post/<title>')
 def post_about(title):
-	post = Post.query.filter_by(title=title).first()
-	return render_template('post.html',posts=[post])
+    post = Post.query.filter_by(title=title).first()
+    return render_template('post.html',posts=[post])
